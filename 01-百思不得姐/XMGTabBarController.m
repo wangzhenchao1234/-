e80f45642be4d0f48b1247a5d39eedd205e24471
“why” <why@163.com>
2015-07-22 09:59:00 +0800

    appearance

--- 01-百思不得姐/XMGTabBarController.m
+++ 01-百思不得姐/XMGTabBarController.m
@@ -14,26 +14,27 @@
 {
     [super viewDidLoad];
     
+    [UINavigationBar appearance];
+    
+    // 通过appearance统一设置所有UITabBarItem的文字属性
+    // 后面带有UI_APPEARANCE_SELECTOR的方法, 都可以通过appearance对象来统一设置
+    NSMutableDictionary *attrs = [NSMutableDictionary dictionary];
+    attrs[NSFontAttributeName] = [UIFont systemFontOfSize:12];
+    attrs[NSForegroundColorAttributeName] = [UIColor grayColor];
+    
+    NSMutableDictionary *selectedAttrs = [NSMutableDictionary dictionary];
+    selectedAttrs[NSFontAttributeName] = attrs[NSFontAttributeName];
+    selectedAttrs[NSForegroundColorAttributeName] = [UIColor darkGrayColor];
+    
+    UITabBarItem *item = [UITabBarItem appearance];
+    [item setTitleTextAttributes:attrs forState:UIControlStateNormal];
+    [item setTitleTextAttributes:selectedAttrs forState:UIControlStateSelected];
+    
     // 添加子控制器
     UIViewController *vc01 = [[UIViewController alloc] init];
     vc01.tabBarItem.title = @"精华";
     vc01.tabBarItem.image = [UIImage imageNamed:@"tabBar_essence_icon"];
     vc01.tabBarItem.selectedImage = [UIImage imageNamed:@"tabBar_essence_click_icon"];
-    
-    NSMutableDictionary *attrs = [NSMutableDictionary dictionary];
-    attrs[NSFontAttributeName] = [UIFont systemFontOfSize:12];
-    attrs[NSForegroundColorAttributeName] = [UIColor grayColor];
-    [vc01.tabBarItem setTitleTextAttributes:attrs forState:UIControlStateNormal];
-    
-    NSMutableDictionary *selectedAttrs = [NSMutableDictionary dictionary];
-    selectedAttrs[NSFontAttributeName] = [UIFont systemFontOfSize:12];
-    selectedAttrs[NSForegroundColorAttributeName] = [UIColor darkGrayColor];
-    [vc01.tabBarItem setTitleTextAttributes:selectedAttrs forState:UIControlStateSelected];
-    
-//    UIImage *image = [UIImage imageNamed:@"tabBar_essence_click_icon"];
-//    image = [image imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal];
-//    vc01.tabBarItem.selectedImage = image;
-    
     vc01.view.backgroundColor = [UIColor redColor];
     [self addChildViewController:vc01];
     
@@ -42,8 +43,6 @@
     vc02.tabBarItem.image = [UIImage imageNamed:@"tabBar_new_icon"];
     vc02.tabBarItem.selectedImage = [UIImage imageNamed:@"tabBar_new_click_icon"];
     vc02.view.backgroundColor = [UIColor grayColor];
-    [vc02.tabBarItem setTitleTextAttributes:attrs forState:UIControlStateNormal];
-    [vc02.tabBarItem setTitleTextAttributes:selectedAttrs forState:UIControlStateSelected];
     [self addChildViewController:vc02];
     
     UIViewController *vc03 = [[UIViewController alloc] init];
@@ -51,8 +50,6 @@
     vc03.tabBarItem.image = [UIImage imageNamed:@"tabBar_friendTrends_icon"];
     vc03.tabBarItem.selectedImage = [UIImage imageNamed:@"tabBar_friendTrends_click_icon"];
     vc03.view.backgroundColor = [UIColor greenColor];
-    [vc03.tabBarItem setTitleTextAttributes:attrs forState:UIControlStateNormal];
-    [vc03.tabBarItem setTitleTextAttributes:selectedAttrs forState:UIControlStateSelected];
     [self addChildViewController:vc03];
     
     UIViewController *vc04 = [[UIViewController alloc] init];
@@ -60,8 +57,6 @@
     vc04.tabBarItem.image = [UIImage imageNamed:@"tabBar_me_icon"];
     vc04.tabBarItem.selectedImage = [UIImage imageNamed:@"tabBar_me_click_icon"];
     vc04.view.backgroundColor = [UIColor blueColor];
-    [vc04.tabBarItem setTitleTextAttributes:attrs forState:UIControlStateNormal];
-    [vc04.tabBarItem setTitleTextAttributes:selectedAttrs forState:UIControlStateSelected];
     [self addChildViewController:vc04];
 }
 @end
