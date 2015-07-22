fe7042b07979d7edc4bce6290afeb0e25dfc8408
“why” <why@163.com>
2015-07-22 09:22:27 +0800

    Initial Commit

---
+++ 01-百思不得姐/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  01-百思不得姐
+//
+//  Created by xiaomage on 15/7/22.
+//  Copyright (c) 2015年 小码哥. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
