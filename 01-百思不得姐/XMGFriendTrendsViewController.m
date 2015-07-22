c3d13de104a27d930327bdcd679f14f02dc38a9f
“why” <why@163.com>
2015-07-22 15:01:30 +0800

    设置导航栏

--- 01-百思不得姐/XMGFriendTrendsViewController.m
+++ 01-百思不得姐/XMGFriendTrendsViewController.m
@@ -14,6 +14,25 @@
 {
     [super viewDidLoad];
     
+    
+    // 设置导航栏标题
+    self.navigationItem.title = @"我的关注";
+//    self.title = @"我的关注";
+//    self.navigationItem.title = @"我的关注";
+//    self.tabBarItem.title = @"我的关注";
+    
+    // 设置导航栏左边的按钮
+    UIButton *friendsButton = [UIButton buttonWithType:UIButtonTypeCustom];
+    [friendsButton setBackgroundImage:[UIImage imageNamed:@"friendsRecommentIcon"] forState:UIControlStateNormal];
+    [friendsButton setBackgroundImage:[UIImage imageNamed:@"friendsRecommentIcon-click"] forState:UIControlStateHighlighted];
+    friendsButton.size = friendsButton.currentBackgroundImage.size;
+    [friendsButton addTarget:self action:@selector(friendsClick) forControlEvents:UIControlEventTouchUpInside];
+    self.navigationItem.leftBarButtonItem = [[UIBarButtonItem alloc] initWithCustomView:friendsButton];
+}
+
+- (void)friendsClick
+{
+    XMGLogFunc;
 }
 
 @end
--- 01-百思不得姐/XMGTabBar.m
+++ 01-百思不得姐/XMGTabBar.m
@@ -21,6 +21,7 @@
         UIButton *publishButton = [UIButton buttonWithType:UIButtonTypeCustom];
         [publishButton setBackgroundImage:[UIImage imageNamed:@"tabBar_publish_icon"] forState:UIControlStateNormal];
         [publishButton setBackgroundImage:[UIImage imageNamed:@"tabBar_publish_click_icon"] forState:UIControlStateHighlighted];
+        publishButton.size = publishButton.currentBackgroundImage.size;
         [self addSubview:publishButton];
         self.publishButton = publishButton;
     }
@@ -34,10 +35,7 @@
     CGFloat width = self.width;
     CGFloat height = self.height;
     
-    
     // 设置发布按钮的frame
-    self.publishButton.width = self.publishButton.currentBackgroundImage.size.width;
-    self.publishButton.height = self.publishButton.currentBackgroundImage.size.height;
     self.publishButton.center = CGPointMake(width * 0.5, height * 0.5);
     
     // 设置其他UITabBarButton的frame
@@ -46,7 +44,6 @@
     CGFloat buttonH = height;
     NSInteger index = 0;
     for (UIView *button in self.subviews) {
-//        if (![button isKindOfClass:NSClassFromString(@"UITabBarButton")]) continue;
         if (![button isKindOfClass:[UIControl class]] || button == self.publishButton) continue;
         
         // 计算按钮的x值
