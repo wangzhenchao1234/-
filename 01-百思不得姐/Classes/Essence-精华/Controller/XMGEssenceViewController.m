c2b2fa7da1e7f56e6666e92ebf74b26e708f87f0
“why” <why@163.com>
2015-07-26 15:40:59 +0800

    标签内容

--- 01-百思不得姐/Classes/Essence-精华/Controller/XMGEssenceViewController.m
+++ 01-百思不得姐/Classes/Essence-精华/Controller/XMGEssenceViewController.m
@@ -8,7 +8,11 @@
 
 #import "XMGEssenceViewController.h"
 #import "XMGRecommendTagsViewController.h"
-#import "XMGTestView.h"
+
+@interface XMGEssenceViewController()
+/** 标签栏底部的红色指示器 */
+@property (nonatomic, weak) UIView *indicatorView;
+@end
 
 @implementation XMGEssenceViewController
 
@@ -16,12 +20,64 @@
 {
     [super viewDidLoad];
     
-    XMGTestView *testView = [[XMGTestView alloc] init];
-    testView.frame = CGRectMake(100, 100, 0, 1000);
-    testView.bounds = CGRectMake(0, 0, 10, 10);
-    testView.backgroundColor = [UIColor redColor];
-    [self.view addSubview:testView];
+    // 设置导航栏
+    [self setupNav];
     
+    // 设置顶部的标签栏
+    [self setupTitlesView];
+}
+
+/**
+ * 设置顶部的标签栏
+ */
+- (void)setupTitlesView
+{
+    // 标签栏整体
+    UIView *titlesView = [[UIView alloc] init];
+    titlesView.backgroundColor = [[UIColor whiteColor] colorWithAlphaComponent:0.7];
+    titlesView.width = self.view.width;
+    titlesView.height = 35;
+    titlesView.y = 64;
+    [self.view addSubview:titlesView];
+    
+    // 内部的子标签
+    NSArray *titles = @[@"全部全部", @"视频", @"声音", @"图片", @"段子"];
+    CGFloat width = titlesView.width / titles.count;
+    CGFloat height = titlesView.height;
+    for (NSInteger i = 0; i<titles.count; i++) {
+        UIButton *button = [[UIButton alloc] init];
+        button.height = height;
+        button.width = width;
+        button.x = i * width;
+        [button setTitle:titles[i] forState:UIControlStateNormal];
+        [button setTitleColor:[UIColor grayColor] forState:UIControlStateNormal];
+        button.titleLabel.font = [UIFont systemFontOfSize:14];
+        [button addTarget:self action:@selector(titleClick:) forControlEvents:UIControlEventTouchUpInside];
+        [titlesView addSubview:button];
+    };
+    
+    // 底部的红色指示器
+    UIView *indicatorView = [[UIView alloc] init];
+    indicatorView.backgroundColor = [UIColor redColor];
+    indicatorView.height = 2;
+    indicatorView.y = titlesView.height - indicatorView.height;
+    [titlesView addSubview:indicatorView];
+    self.indicatorView = indicatorView;
+}
+
+- (void)titleClick:(UIButton *)button
+{
+    [UIView animateWithDuration:0.25 animations:^{
+        self.indicatorView.width = button.titleLabel.width;
+        self.indicatorView.centerX = button.centerX;
+    }];
+}
+
+/**
+ * 设置导航栏
+ */
+- (void)setupNav
+{
     // 设置导航栏标题
     self.navigationItem.titleView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"MainTitle"]];
     
