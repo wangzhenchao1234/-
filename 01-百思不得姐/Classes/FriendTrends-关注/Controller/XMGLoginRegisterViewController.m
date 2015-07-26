c3efd2fdc8c9a97123e8200534d220d2bdb3663d
“why” <why@163.com>
2015-07-26 14:37:26 +0800

    注册框

--- 01-百思不得姐/Classes/FriendTrends-关注/Controller/XMGLoginRegisterViewController.m
+++ 01-百思不得姐/Classes/FriendTrends-关注/Controller/XMGLoginRegisterViewController.m
@@ -9,19 +9,36 @@
 #import "XMGLoginRegisterViewController.h"
 
 @interface XMGLoginRegisterViewController ()
-
+/** 登录框距离控制器view左边的间距 */
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *loginViewLeftMargin;
 @end
 
 @implementation XMGLoginRegisterViewController
+
+- (IBAction)back {
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (IBAction)showLoginOrRegister:(UIButton *)button {
+    // 退出键盘
+    [self.view endEditing:YES];
+    
+    if (self.loginViewLeftMargin.constant == 0) { // 显示注册界面
+        self.loginViewLeftMargin.constant = - self.view.width;
+        button.selected = YES;
+    } else { // 显示登录界面
+        self.loginViewLeftMargin.constant = 0;
+        button.selected = NO;
+    }
+    
+    [UIView animateWithDuration:0.25 animations:^{
+        [self.view layoutIfNeeded];
+    }];
 }
 
 /**
--- 01-百思不得姐/Classes/FriendTrends-关注/View/XMGVerticalButton.m
+++ 01-百思不得姐/Classes/FriendTrends-关注/View/XMGVerticalButton.m
@@ -28,21 +28,6 @@
     [self setup];
 }
 
-//- (void)layoutSubviews
-//{
-//    [super layoutSubviews];
-//    
-//    // 调整图片
-//    self.imageView.x = 0;
-//    self.imageView.y = 0;
-//    
-//    // 调整文字
-//    self.titleLabel.x = 0;
-//    self.titleLabel.y = self.imageView.height;
-//    self.titleLabel.width = self.width;
-//    self.titleLabel.height = self.height - self.titleLabel.y;
-//}
-
 - (void)layoutSubviews
 {
     [super layoutSubviews];
