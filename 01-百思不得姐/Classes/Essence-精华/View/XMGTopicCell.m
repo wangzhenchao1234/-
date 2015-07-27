c9da79eacfded8bbaeec637e012b2a4018fb7e83
“why” <why@163.com>
2015-07-27 14:53:30 +0800

    日期显示

--- 01-百思不得姐/Classes/Essence-精华/View/XMGTopicCell.m
+++ 01-百思不得姐/Classes/Essence-精华/View/XMGTopicCell.m
@@ -37,13 +37,35 @@
     self.backgroundView = bgView;
 }
 
+/**
+ 今年
+    今天
+        1分钟内
+            刚刚
+        1小时内
+            xx分钟前
+        其他
+            xx小时前
+    昨天
+        昨天 18:56:34
+    其他
+        06-23 19:56:23
+ 
+ 非今年
+    2014-05-08 18:45:30
+ */
+
 - (void)setTopic:(XMGTopic *)topic
 {
     _topic = topic;
     
-    // 设置其他控件
+    // 设置头像
     [self.profileImageView sd_setImageWithURL:[NSURL URLWithString:topic.profile_image] placeholderImage:[UIImage imageNamed:@"defaultUserIcon"]];
+    
+    // 设置名字
     self.nameLabel.text = topic.name;
+    
+    // 设置帖子的创建时间
     self.createTimeLabel.text = topic.create_time;
     
     // 设置按钮文字
@@ -51,54 +73,7 @@
     [self setupButtonTitle:self.caiButton count:topic.cai placeholder:@"踩"];
     [self setupButtonTitle:self.shareButton count:topic.repost placeholder:@"分享"];
     [self setupButtonTitle:self.commentButton count:topic.comment placeholder:@"评论"];
-    
-//    [self testDate:topic.create_time];
 }
-
-- (void)testDate:(NSString *)create_time
-{
-    // 日期格式化类
-    NSDateFormatter *fmt = [[NSDateFormatter alloc] init];
-    // 设置日期格式(y:年,M:月,d:日,H:时,m:分,s:秒)
-    fmt.dateFormat = @"yyyy-MM-dd HH:mm:ss";
-    
-    // 当前时间
-    NSDate *now = [NSDate date];
-    // 发帖时间
-    NSDate *create = [fmt dateFromString:create_time];
-    
-    XMGLog(@"%@", [now deltaFrom:create]);
-    
-//    // 日历
-//    NSCalendar *calendar = [NSCalendar currentCalendar];
-//    
-//    // 比较时间
-//    NSCalendarUnit unit = NSCalendarUnitDay | NSCalendarUnitMonth | NSCalendarUnitYear | NSCalendarUnitHour | NSCalendarUnitMinute | NSCalendarUnitSecond;
-//    NSDateComponents *cmps = [calendar components:unit fromDate:create toDate:now options:0];
-//    
-//    XMGLog(@"%@ %@", create, now);
-//    XMGLog(@"%zd %zd %zd %zd %zd %zd", cmps.year, cmps.month, cmps.day, cmps.hour, cmps.minute, cmps.second);
-    
-    // 获得NSDate的每一个元素
-//    NSInteger year = [calendar component:NSCalendarUnitYear fromDate:now];
-//    NSInteger month = [calendar component:NSCalendarUnitMonth fromDate:now];
-//    NSInteger day = [calendar component:NSCalendarUnitDay fromDate:now];
-//    NSDateComponents *cmps = [calendar components:NSCalendarUnitDay | NSCalendarUnitMonth | NSCalendarUnitYear fromDate:now];
-//    XMGLog(@"%zd %zd %zd", cmps.year, cmps.month, cmps.day);
-}
-
-//- (void)testDate:(NSString *)create_time
-//{
-//    // 当前时间
-//    NSDate *now = [NSDate date];
-//    
-//    // 发帖时间
-//    NSDateFormatter *fmt = [[NSDateFormatter alloc] init];
-//    // 设置日期格式(y:年,M:月,d:日,H:时,m:分,s:秒)
-//    fmt.dateFormat = @"yyyy-MM-dd HH:mm:ss";
-//    NSDate *create = [fmt dateFromString:create_time];
-//    NSTimeInterval delta = [now timeIntervalSinceDate:create];
-//}
 
 /**
  * 设置底部按钮文字
--- 01-百思不得姐/Classes/Other-其他/Category/NSDate+XMGExtension.h
+++ 01-百思不得姐/Classes/Other-其他/Category/NSDate+XMGExtension.h
@@ -13,4 +13,19 @@
  * 比较from和self的时间差值
  */
 - (NSDateComponents *)deltaFrom:(NSDate *)from;
+
+/**
+ * 是否为今年
+ */
+- (BOOL)isThisYear;
+
+/**
+ * 是否为今天
+ */
+- (BOOL)isToday;
+
+/**
+ * 是否为昨天
+ */
+- (BOOL)isYesterday;
 @end
