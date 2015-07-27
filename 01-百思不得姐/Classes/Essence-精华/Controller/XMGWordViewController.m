066af21e15e89af6f09fe9a44f2e9635bcd1e41b
“why” <why@163.com>
2015-07-27 09:54:29 +0800

    下拉上拉

--- 01-百思不得姐/Classes/Essence-精华/Controller/XMGWordViewController.m
+++ 01-百思不得姐/Classes/Essence-精华/Controller/XMGWordViewController.m
@@ -9,31 +9,139 @@
 #import "XMGWordViewController.h"
 #import <AFNetworking.h>
 #import <UIImageView+WebCache.h>
+#import "XMGTopic.h"
+#import <MJExtension.h>
+#import <MJRefresh.h>
 
 @interface XMGWordViewController ()
 /** 帖子数据 */
-@property (nonatomic, strong) NSArray *topics;
+@property (nonatomic, strong) NSMutableArray *topics;
+/** 当前页码 */
+@property (nonatomic, assign) NSInteger page;
+/** 当加载下一页数据时需要这个参数 */
+@property (nonatomic, copy) NSString *maxtime;
+/** 上一次的请求参数 */
+@property (nonatomic, strong) NSDictionary *params;
 @end
 
 @implementation XMGWordViewController
 
+- (NSMutableArray *)topics
+{
+    if (!_topics) {
+        _topics = [NSMutableArray array];
+    }
+    return _topics;
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    // 添加刷新控件
+    [self setupRefresh];
+}
+
+- (void)setupRefresh
+{
+    self.tableView.header = [MJRefreshNormalHeader headerWithRefreshingTarget:self refreshingAction:@selector(loadNewTopics)];
+    // 自动改变透明度
+    self.tableView.header.autoChangeAlpha = YES;
+    [self.tableView.header beginRefreshing];
+    
+    self.tableView.footer = [MJRefreshBackNormalFooter footerWithRefreshingTarget:self refreshingAction:@selector(loadMoreTopics)];
+}
+
+#pragma mark - 数据处理
+/**
+ * 加载新的帖子数据
+ */
+- (void)loadNewTopics
+{
+    // 结束上啦
+    [self.tableView.footer endRefreshing];
     
     // 参数
     NSMutableDictionary *params = [NSMutableDictionary dictionary];
     params[@"a"] = @"list";
     params[@"c"] = @"data";
     params[@"type"] = @"29";
+    self.params = params;
     
     // 发送请求
     [[AFHTTPSessionManager manager] GET:@"http://api.budejie.com/api/api_open.php" parameters:params success:^(NSURLSessionDataTask *task, NSDictionary *responseObject) {
-        self.topics = responseObject[@"list"];
+        if (self.params != params) return;
         
+        // 存储maxtime
+        self.maxtime = responseObject[@"info"][@"maxtime"];
+        
+        // 字典 -> 模型
+        self.topics = [XMGTopic objectArrayWithKeyValuesArray:responseObject[@"list"]];
+        
+        // 刷新表格
         [self.tableView reloadData];
-//        [responseObject writeToFile:@"/Users/xiaomage/Desktop/duanzi.plist" atomically:YES];
-    } failure:^(NSURLSessionDataTask *task, NSError *error) {
         
+        // 结束刷新
+        [self.tableView.header endRefreshing];
+        
+        // 清空页码
+        self.page = 0;
+    } failure:^(NSURLSessionDataTask *task, NSError *error) {
+        if (self.params != params) return;
+        
+        // 结束刷新
+        [self.tableView.header endRefreshing];
+    }];
+}
+
+// 先下拉刷新, 再上拉刷新第5页数据
+
+// 下拉刷新成功回来: 只有一页数据, page == 0
+// 上啦刷新成功回来: 最前面那页 + 第5页数据
+
+/**
+ * 加载更多的帖子数据
+ */
+- (void)loadMoreTopics
+{
+    return;
+    // 结束下拉
+    [self.tableView.header endRefreshing];
+    
+    self.page++;
+    
+    // 参数
+    NSMutableDictionary *params = [NSMutableDictionary dictionary];
+    params[@"a"] = @"list";
+    params[@"c"] = @"data";
+    params[@"type"] = @"29";
+    params[@"page"] = @(self.page);
+    params[@"maxtime"] = self.maxtime;
+    self.params = params;
+    
+    // 发送请求
+    [[AFHTTPSessionManager manager] GET:@"http://api.budejie.com/api/api_open.php" parameters:params success:^(NSURLSessionDataTask *task, NSDictionary *responseObject) {
+        if (self.params != params) return;
+        
+        // 存储maxtime
+        self.maxtime = responseObject[@"info"][@"maxtime"];
+        
+        // 字典 -> 模型
+        NSArray *newTopics = [XMGTopic objectArrayWithKeyValuesArray:responseObject[@"list"]];
+        [self.topics addObjectsFromArray:newTopics];
+        
+        // 刷新表格
+        [self.tableView reloadData];
+        
+        // 结束刷新
+        [self.tableView.footer endRefreshing];
+    } failure:^(NSURLSessionDataTask *task, NSError *error) {
+        if (self.params != params) return;
+        
+        // 结束刷新
+        [self.tableView.footer endRefreshing];
+        
+        // 恢复页码
+        self.page--;
     }];
 }
 
@@ -52,66 +160,12 @@
         cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:ID];
     }
     
-    NSDictionary *topic = self.topics[indexPath.row];
-    cell.textLabel.text = topic[@"name"];
-    cell.detailTextLabel.text = topic[@"text"];
-    [cell.imageView sd_setImageWithURL:[NSURL URLWithString:topic[@"profile_image"]] placeholderImage:[UIImage imageNamed:@"defaultUserIcon"]];
+    XMGTopic *topic = self.topics[indexPath.row];
+    cell.textLabel.text = topic.name;
+    cell.detailTextLabel.text = topic.text;
+    [cell.imageView sd_setImageWithURL:[NSURL URLWithString:topic.profile_image] placeholderImage:[UIImage imageNamed:@"defaultUserIcon"]];
     
     return cell;
 }
-
-/*
-- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:<#@"reuseIdentifier"#> forIndexPath:indexPath];
-    
-    // Configure the cell...
-    
-    return cell;
-}
-*/
-
-/*
-// Override to support conditional editing of the table view.
-- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
-    // Return NO if you do not want the specified item to be editable.
-    return YES;
-}
-*/
-
-/*
-// Override to support editing the table view.
-- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
-    if (editingStyle == UITableViewCellEditingStyleDelete) {
-        // Delete the row from the data source
-        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
-    } else if (editingStyle == UITableViewCellEditingStyleInsert) {
-        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-    }   
-}
-*/
-
-/*
-// Override to support rearranging the table view.
-- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath {
-}
-*/
-
-/*
-// Override to support conditional rearranging of the table view.
-- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath {
-    // Return NO if you do not want the item to be re-orderable.
-    return YES;
-}
-*/
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
