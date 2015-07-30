f9dd536ad816937ffb69d919acd1c379a9ee018f
“why” <why@163.com>
2015-07-30 09:07:29 +0800

    发布界面

--- 01-百思不得姐/Classes/Essence-精华/Controller/XMGShowPictureViewController.m
+++ 01-百思不得姐/Classes/Essence-精华/Controller/XMGShowPictureViewController.m
@@ -23,10 +23,6 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    // 屏幕尺寸
-    CGFloat screenH = [UIScreen mainScreen].bounds.size.height;
-    CGFloat screenW = [UIScreen mainScreen].bounds.size.width;
-    
     // 添加图片
     UIImageView *imageView = [[UIImageView alloc] init];
     imageView.userInteractionEnabled = YES;
@@ -35,14 +31,14 @@
     self.imageView = imageView;
     
     // 图片尺寸
-    CGFloat pictureW = screenW;
+    CGFloat pictureW = XMGScreenW;
     CGFloat pictureH = pictureW * self.topic.height / self.topic.width;
-    if (pictureH > screenH) { // 图片显示高度超过一个屏幕, 需要滚动查看
+    if (pictureH > XMGScreenH) { // 图片显示高度超过一个屏幕, 需要滚动查看
         imageView.frame = CGRectMake(0, 0, pictureW, pictureH);
         self.scrollView.contentSize = CGSizeMake(0, pictureH);
     } else {
         imageView.size = CGSizeMake(pictureW, pictureH);
-        imageView.centerY = screenH * 0.5;
+        imageView.centerY = XMGScreenH * 0.5;
     }
     
     // 马上显示当前图片的下载进度
