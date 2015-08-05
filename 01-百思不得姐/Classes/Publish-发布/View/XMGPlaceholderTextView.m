ab8524ef7b6727bfce5077e17dc6a8359df74f1c
“why” <why@163.com>
2015-08-05 11:12:16 +0800

    label实现占位文字

--- 01-百思不得姐/Classes/Publish-发布/View/XMGPlaceholderTextView.m
+++ 01-百思不得姐/Classes/Publish-发布/View/XMGPlaceholderTextView.m
@@ -8,7 +8,27 @@
 
 #import "XMGPlaceholderTextView.h"
 
+@interface XMGPlaceholderTextView()
+/** 占位文字label */
+@property (nonatomic, weak) UILabel *placeholderLabel;
+@end
+
 @implementation XMGPlaceholderTextView
+
+- (UILabel *)placeholderLabel
+{
+    if (!_placeholderLabel) {
+        // 添加一个用来显示占位文字的label
+        UILabel *placeholderLabel = [[UILabel alloc] init];
+        placeholderLabel.numberOfLines = 0;
+        placeholderLabel.x = 4;
+        placeholderLabel.y = 7;
+        [self addSubview:placeholderLabel];
+        _placeholderLabel = placeholderLabel;
+//        self.placeholderLabel = placeholderLabel; // setter方法
+    }
+    return _placeholderLabel;
+}
 
 - (instancetype)initWithFrame:(CGRect)frame
 {
@@ -38,27 +58,17 @@
  */
 - (void)textDidChange
 {
-    [self setNeedsDisplay];
+    // 只要有文字, 就隐藏占位文字label
+    self.placeholderLabel.hidden = self.hasText;
 }
 
 /**
- * 绘制占位文字(每次drawRect:之前, 会自动清除掉之前绘制的内容)
+ * 更新占位文字的尺寸
  */
-- (void)drawRect:(CGRect)rect {
-    // 如果有文字, 直接返回, 不绘制占位文字
-//    if (self.text.length || self.attributedText.length) return;
-    if (self.hasText) return;
-    
-    // 处理rect
-    rect.origin.x = 4;
-    rect.origin.y = 7;
-    rect.size.width -= 2 * rect.origin.x;
-    
-    // 文字属性
-    NSMutableDictionary *attrs = [NSMutableDictionary dictionary];
-    attrs[NSFontAttributeName] = self.font;
-    attrs[NSForegroundColorAttributeName] = self.placeholderColor;
-    [self.placeholder drawInRect:rect withAttributes:attrs];
+- (void)updatePlaceholderLabelSize
+{
+    CGSize maxSize = CGSizeMake(XMGScreenW - 2 * self.placeholderLabel.x, MAXFLOAT);
+    self.placeholderLabel.size = [self.placeholder boundingRectWithSize:maxSize options:NSStringDrawingUsesLineFragmentOrigin attributes:@{NSFontAttributeName : self.font} context:nil].size;
 }
 
 #pragma mark - 重写setter
@@ -66,35 +76,39 @@
 {
     _placeholderColor = placeholderColor;
     
-    [self setNeedsDisplay];
+    self.placeholderLabel.textColor = placeholderColor;
 }
 
 - (void)setPlaceholder:(NSString *)placeholder
 {
     _placeholder = [placeholder copy];
     
-    [self setNeedsDisplay];
+    self.placeholderLabel.text = placeholder;
+    
+    [self updatePlaceholderLabelSize];
 }
 
 - (void)setFont:(UIFont *)font
 {
     [super setFont:font];
     
-    [self setNeedsDisplay];
+    self.placeholderLabel.font = font;
+    
+    [self updatePlaceholderLabelSize];
 }
 
 - (void)setText:(NSString *)text
 {
     [super setText:text];
     
-    [self setNeedsDisplay];
+    [self textDidChange];
 }
 
 - (void)setAttributedText:(NSAttributedString *)attributedText
 {
     [super setAttributedText:attributedText];
     
-    [self setNeedsDisplay];
+    [self textDidChange];
 }
 
 @end
